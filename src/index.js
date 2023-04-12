1b05f833296cd60b07dade5eac6113fb19bbb1a5
farnaz garehdaghi <farnazgarehdaghi@gmail.com>
2023-04-12 09:42:30 -0700

    dayname

---
+++ src/index.js
@@ -0,0 +1,13 @@
+let today = new Date();
+let dayname1 = today.getDay();
+let daysofWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Fraiday",
+  "Saturday",
+];
+let dayVal = daysofWeek[dayname1];
+let dayname2 = document.querySelector(".dayName");
